e6f09ec82dbacef05152e1f2ef5352279a2c5cf2
Dennis Hedegaard <dennis@dhedegaard.dk>
2018-12-05 20:31:54 +0100

    main: Added a table for showing the results.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,9 @@
+#[macro_use]
+extern crate prettytable;
 extern crate time;
+
+use prettytable::format;
+use prettytable::Table;
 
 mod day01;
 mod day02;
@@ -7,74 +12,80 @@ mod day04;
 mod day05;
 
 fn main() {
+    let mut table = Table::new();
+    table.set_format(*format::consts::FORMAT_NO_LINESEP_WITH_TITLE);
+    table.set_titles(row!["Day", "Part", "Result", "Duration"]);
+
     {
         let input = day01::parse_input(&day01::raw_input());
         let before = time::now();
         let result = day01::part1(&input);
         let diff = time::now() - before;
-        println!("day01,\tpart1: {},\ttook {}", result, diff);
+        table.add_row(row![1, 1, result, diff]);
     }
     {
         let input = day01::parse_input(&day01::raw_input());
         let before = time::now();
         let result = day01::part2(&input);
         let diff = time::now() - before;
-        println!("day01,\tpart2: {},\ttook {}", result, diff);
+        table.add_row(row![1, 2, result, diff]);
     }
     {
         let input = day02::get_input();
         let before = time::now();
         let result = day02::part1(&input);
         let diff = time::now() - before;
-        println!("day02,\tpart1: {},\ttook {}", result, diff);
+        table.add_row(row![2, 1, result, diff]);
     }
     {
         let input = day02::get_input();
         let before = time::now();
         let result = day02::part2(&input);
         let diff = time::now() - before;
-        println!("day02,\tpart2: {},\ttook {}", result, diff);
+        table.add_row(row![2, 2, result, diff]);
     }
     {
         let input = day03::parse_input(&day03::get_input());
         let before = time::now();
         let result = day03::part1(&input);
         let diff = time::now() - before;
-        println!("day03,\tpart1: {},\ttook {}", result, diff);
+        table.add_row(row![3, 1, result, diff]);
     }
     {
         let input = day03::parse_input(&day03::get_input());
         let before = time::now();
         let result = day03::part2(&input);
         let diff = time::now() - before;
-        println!("day03,\tpart2: {},\ttook {}", result, diff);
+        table.add_row(row![3, 2, result, diff]);
     }
     {
         let input = day04::parse_input(&day04::get_input());
         let before = time::now();
         let result = day04::part1(&input);
         let diff = time::now() - before;
-        println!("day04,\tpart1: {},\ttook {}", result, diff);
+        table.add_row(row![4, 1, result, diff]);
     }
     {
         let input = day04::parse_input(&day04::get_input());
         let before = time::now();
         let result = day04::part2(&input);
         let diff = time::now() - before;
-        println!("day04,\tpart2: {},\ttook {}", result, diff);
+        table.add_row(row![4, 2, result, diff]);
     }
     {
         let input = day05::get_input();
         let before = time::now();
         let result = day05::part1(&input).len();
         let diff = time::now() - before;
-        println!("day05,\tpart1: {},\ttook {}", result, diff);
+        table.add_row(row![5, 1, result, diff]);
     }
     {
         let input = day05::get_input();
         let before = time::now();
         let result = day05::part2(&input);
         let diff = time::now() - before;
-        println!("day05,\tpart2: {},\ttook {}", result, diff);
+        table.add_row(row![5, 2, result, diff]);
     }
+
+    table.printstd();
 }
